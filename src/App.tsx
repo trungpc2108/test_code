9876f190745584a93ba6ae8b49a84e32f8e9bf4d
kieulong004 <longvankieu2004@gmail.com>
2024-09-29 23:23:35 +0700

    add

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Layout from './components/layout'
+import Page from './components/page'
 
 function App() {
 
@@ -9,7 +10,7 @@ function App() {
     <>
       <Routes >
         <Route path='' element={<Layout />}>
-          
+          <Route path='page' element={<Page />}/>
         </Route>
       </Routes>
     </>
