f8dcd5465855c41773ce63ce04f2f68a2091cf56
nguyenxuanhung <hungnxph35478@fpt.edu.vn>
2024-09-30 15:23:40 +0700

    codeok

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Layout from './components/layout'
+import Test123 from './components/test123'
 
 function App() {
 
@@ -9,7 +10,7 @@ function App() {
     <>
       <Routes >
         <Route path='' element={<Layout />}>
-          
+          <Route path='test123' element={<Test123 />} />
         </Route>
       </Routes>
     </>
---
+++ src/components/test123.tsx
@@ -0,0 +1,8 @@
+
+const Test123 = () => {
+  return (
+    <div>test123</div>
+  )
+}
+
+export default Test123
