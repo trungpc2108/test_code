a72dc8e7d7720a63e2a6dffcef2b28503b687086
trungpc2108 <150658199+trungpc2108@users.noreply.github.com>
2024-09-30 15:30:23 +0700

    caws
    sdaada

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import Layout from './components/layout'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Layout from "./components/layout";
+import Ctdd from "./components/ctdd";
 
 function App() {
-
   return (
     <>
-      <Routes >
-        <Route path='' element={<Layout />}>
-          
+      <Routes>
+        <Route path="" element={<Layout />}>
+          <Route path="ctdd" element={<Ctdd />} />
         </Route>
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
